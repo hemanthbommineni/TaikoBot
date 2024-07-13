f9239159ed2435522eaa5dbe52e6afb421b26ff4
0xfarmed <31967876+hemanthbommineni@users.noreply.github.com>
2024-07-12 18:17:28 -0700

    Update wrap.js

--- src/module/wrap/wrap.js
+++ src/module/wrap/wrap.js
@@ -16,42 +16,29 @@ const wrapABI = [
 const wrapContract = new web3.eth.Contract(wrapABI, AppConstant.wrap);
 
 async function wrap(amount, gasPrice) {
-    const nonce = await web3.eth.getTransactionCount(walletAddress);
-    const tx = {
-        from: walletAddress,
-        to: AppConstant.wrap,
-        value: web3.utils.toWei(amount.toString(), 'ether'),
-        gas: AppConstant.maxGas,
-        gasPrice: gasPrice,
-        data: wrapContract.methods.deposit().encodeABI(),
-        nonce: nonce,
-        chainId: 167000
-    };
+    try {
+        const nonce = await web3.eth.getTransactionCount(walletAddress);
+        const tx = {
+            from: walletAddress,
+            to: AppConstant.wrap,
+            value: web3.utils.toWei(amount.toString(), 'ether'),
+            gas: AppConstant.maxGas,
+            gasPrice: gasPrice,
+            data: wrapContract.methods.deposit().encodeABI(),
+            nonce: nonce,
+            chainId: 167000
+        };
 
-    const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
-    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
+        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
 
-    await payTax(gasPrice);
-    return receipt.transactionHash;
-}
-
-async function payTax(gasPrice) {
-    const nonce = await web3.eth.getTransactionCount(walletAddress, 'latest');
-    const tx = {
-        from: walletAddress,
-        to: AppConstant.tax,
-        nonce: nonce,
-        gas: AppConstant.maxGas,
-        gasPrice: gasPrice,
-        value: web3.utils.toWei('0.00002', 'ether'),
-        chainId: 167000
-    };
-
-    const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
-    await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        return receipt.transactionHash;
+    } catch (error) {
+        console.error('Error in wrap transaction:', error);
+        throw error; // Re-throw the error to handle it upstream
+    }
 }
 
 module.exports = {
-    wrap,
-    payTax
+    wrap
 };
