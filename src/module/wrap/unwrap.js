78791992d44cc76e69cfd2bf54205aa66b023d01
0xfarmed <31967876+hemanthbommineni@users.noreply.github.com>
2024-07-12 18:18:18 -0700

    Update unwrap.js

--- src/module/wrap/unwrap.js
+++ src/module/wrap/unwrap.js
@@ -20,42 +20,31 @@ const contractABI = [
     }
 ];
 
-const contract = new web3.eth.Contract(contractABI, AppConstant.wrap);
+const contractAddress = process.env.CONTRACT_ADDRESS; // Replace with your contract address
+const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-async function unwrap(amount, gasPrice, nonce) {
-    const amountWei = web3.utils.toWei(amount.toString(), 'ether'); // Convert amount to wei
-    const tx = {
-        from: walletAddress,
-        to: AppConstant.wrap,
-        gas: AppConstant.maxGas,
-        gasPrice: gasPrice,
-        data: contract.methods.withdraw(amountWei).encodeABI(),
-        nonce: nonce,
-        chainId: 167000
-    };
+async function unwrap(amount, gasPrice) {
+    try {
+        const nonce = await web3.eth.getTransactionCount(walletAddress);
+        const amountWei = web3.utils.toWei(amount.toString(), 'ether');
+        const tx = {
+            from: walletAddress,
+            to: contractAddress,
+            gas: AppConstant.maxGas,
+            gasPrice: gasPrice,
+            data: contract.methods.withdraw(amountWei).encodeABI(),
+            nonce: nonce,
+            chainId: 167000
+        };
 
-    const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
-    const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-    
-    // Pay tax
-    await payTax(gasPrice, nonce + 1);
+        const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
+        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
 
-    return receipt.transactionHash;
-}
-
-async function payTax(gasPrice, nonce) {
-    const tx = {
-        from: walletAddress,
-        to: AppConstant.tax,
-        value: web3.utils.toWei('0.00002', 'ether'),
-        gas: AppConstant.maxGas,
-        gasPrice: gasPrice,
-        nonce: nonce,
-        chainId: 167000
-    };
-
-    const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
-    await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        return receipt.transactionHash;
+    } catch (error) {
+        console.error('Error in unwrap transaction:', error);
+        throw error; // Re-throw the error to handle it upstream
+    }
 }
 
 module.exports = {
