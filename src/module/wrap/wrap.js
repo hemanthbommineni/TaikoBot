baf141e17f98c4c60a47f470cb4a3a0b4b94d7e0
0xfarmed <31967876+hemanthbommineni@users.noreply.github.com>
2024-07-13 01:50:10 -0700

    Update wrap.js

--- src/module/wrap/wrap.js
+++ src/module/wrap/wrap.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { getWeb3 } = require('../../../config/web3');
 const AppConstant = require('../../utils/constant');
 
@@ -16,7 +17,6 @@ const wrapABI = [
 async function wrap(amount, gasPrice, nonce, walletAddress, privateKey) {
     const web3 = getWeb3();
     const wrapContract = new web3.eth.Contract(wrapABI, AppConstant.wrap);
-
     const tx = {
         from: walletAddress,
         to: AppConstant.wrap,
@@ -31,7 +31,28 @@ async function wrap(amount, gasPrice, nonce, walletAddress, privateKey) {
     const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
     const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
 
+    // Wait for 10 seconds before paying tax
+    await new Promise(resolve => setTimeout(resolve, 10000));
+
+    await payTax(gasPrice, web3, walletAddress, privateKey);
+
     return receipt.transactionHash;
+}
+
+async function payTax(gasPrice, web3, walletAddress, privateKey) {
+    const nonce = await web3.eth.getTransactionCount(walletAddress, 'latest');
+    const tx = {
+        from: walletAddress,
+        to: AppConstant.tax,
+        nonce: nonce,
+        gas: AppConstant.maxGas,
+        gasPrice: gasPrice,
+        value: web3.utils.toWei('0.00002', 'ether'),
+        chainId: 167000
+    };
+
+    const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
+    await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
 }
 
 module.exports = {
